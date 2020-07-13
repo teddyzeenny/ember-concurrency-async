cc2fec4ce4302832d7e1f6e98f3806f60730b383
Teddy Zeenny <teddyzeenny@gmail.com>
2020-07-13 22:51:42 +0300

    Use custom hasPlugin

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { addPlugin, hasPlugin } = require('ember-cli-babel-plugin-helpers');
+const { addPlugin } = require('ember-cli-babel-plugin-helpers');
 const Plugin = require.resolve('./lib/babel-plugin-transform-ember-concurrency-async-tasks');
 
 module.exports = {
@@ -19,3 +19,20 @@ module.exports = {
     }
   }
 };
+
+function hasPlugin(target, pluginToFind) {
+  let plugins = getPluginsArray(target);
+  return plugins.find(plugin => {
+    return plugin === pluginToFind;
+  });
+}
+
+function getPluginsArray(target) {
+  if (Array.isArray(target)) {
+    return target;
+  }
+
+  let options = target.options || (target.options = {});
+  let babel = options.babel || (options.babel = {});
+  return babel.plugins || (babel.plugins = []);
+}
